62e1e4194018f0596c339dad37e0e1ff76008018
GitHubのユーザ名 <GitHub>
2022-04-01 13:28:24 +0000

    [FINISH]ログイン画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 Rails.application.routes.draw do
 
-  root :to => 'homes#top'
-  get 'homes/about'
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/create'
+    get 'items/show'
+    get 'items/edit'
+    get 'items/update'
+  end
+  namespace :admin do
+
+    resources :genres, only: [:index, :create, :edit, :update]
+
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+
+  end
+  get '/admin' => 'admin/homes#top'
+
+  root 'public/homes#top'
+  get '/about' => 'public/homes#about'
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords],  controllers: {
@@ -12,7 +29,7 @@ Rails.application.routes.draw do
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
+    sessions: "admin/sessions"
   }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
