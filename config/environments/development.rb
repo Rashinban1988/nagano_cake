7584cc12911e2629cc9de8dfa03b151d2df896b6
GitHubのユーザ名 <GitHub>
2022-04-01 11:38:12 +0000

    [START]ログイン画面

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "c21f285d9b1c420380012f9fa3340b9d.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
